00251aa4a6ae08d38ce41551976216ba34a51f7a
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2019-11-05 22:12:29 +0100

    Clean up main

--- mailclient.py
+++ mailclient.py
@@ -1,19 +1,17 @@
 from __future__ import print_function
-import pprint
 import pickle
 import os.path
 import sqlite3
 import datetime
-from pprint import pprint
 import re
-
+import time
+from pprint import pprint
+from sqlite import create_db, insert_event, get_event, get_events
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-from sqlite import create_db, insert_event, get_event, get_events
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
@@ -21,15 +19,18 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 # Converts the timestamp in mail for dispenseEvent to unix timestamp
 def getUnixTime(time):
+    # Get all numbers from the snippet
     array = list(map(int, re.findall(r'[0-9]+', time)))
+    # Convert to unix timestamp in correct order. Might be a better solution.
+    # (mail is in DD/MM/YY, function is in (YY/MM/DD, therefore the weird indexes)
     return int(datetime.datetime(array[2], array[1], array[0], array[3], array[4], array[5]).timestamp())
 
 
 def getDrink(event):
-    return re.findall("(?<=;)[a-zA-Z ]*(?=&)", event)[0]
+    return re.findall("(?<=;)[a-zA-Z é]*(?=&)", event)[0]
 
 
-def main():
+def setupConnection():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -49,20 +50,34 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('gmail', 'v1', credentials=creds)
+    return build('gmail', 'v1', credentials=creds)
 
-    #TODO mark emails as unread. Or find better solution than to list all mails, perhaps also delete already-processed mails?
-    inbox = service.users().messages().list(userId='me', q='label:dispenseddrinkevent is:unread', maxResults=10).execute()
-    mails = [service.users().messages().get(userId="me", id=msg['id'], format="minimal").execute() for msg in
+
+def addAllUnreadDispensedDrinkEvent(gmail_con, db_conn):
+    # TODO mark emails as unread. Or find better solution than to list all mails, perhaps also delete already-processed mails?
+    # Get all mails that are unread and have label dispenseddrinkevent. All mails containing the words "DispensedDrinkEvent"
+    # automatically gets this label when received
+    inbox = gmail_con.users().messages().list(userId='me', q='label:dispenseddrinkevent is:unread').execute()
+    # Gets information from mails in "minimal" format, as only a snippet of the mail is needed (recent event is in start of mail)
+    mails = [gmail_con.users().messages().get(userId="me", id=msg['id'], format="minimal").execute() for msg in
              inbox["messages"]]
 
-    db_conn = sqlite3.connect('jvm-loss.db')
-    create_db(db_conn, True, True)
-
+    # Add to db
     for m in mails:
         insert_event(db_conn, getUnixTime(m["snippet"]), "DispensedDrinkEvent", getDrink(m["snippet"]))
 
-    pprint(get_events(db_conn))
+
+def main():
+    gmail_con = setupConnection()
+
+    # Create database
+    db_conn = sqlite3.connect('jvm-loss.db')
+    create_db(db_conn, True, True)
+
+    # Get all unread DispensedDrinkEvent and add to database
+    while True:
+        addAllUnreadDispensedDrinkEvent(gmail_con, db_conn)
+        time.sleep(600)
 
 
 if __name__ == '__main__':
