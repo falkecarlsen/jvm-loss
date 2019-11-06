80ce356d5a60075b556cdfa54d412e51f20ba740
Mathias Sørensen <mathiasmehlsoerensen@gmail.com>
2019-11-06 23:44:57 +0100

    Add batch modify, reformat code

--- mailclient.py
+++ mailclient.py
@@ -5,31 +5,18 @@ import sqlite3
 import datetime
 import re
 import time
-from pprint import pprint
 from sqlite import create_db, insert_event, get_event, get_events
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 
 # TODO hvad hvis folk kommer til at købe noget, standse ved en fejl, og så tage den igen. Det tæller som 2 køb, men gælder faktisk kun for 1.
 
-# Converts the timestamp in mail for dispenseEvent to unix timestamp
-def getUnixTime(time):
-    # Get all numbers from the snippet
-    array = list(map(int, re.findall(r'[0-9]+', time)))
-    # Convert to unix timestamp in correct order. Might be a better solution.
-    # (mail is in DD/MM/YY, function is in (YY/MM/DD, therefore the weird indexes)
-    return int(datetime.datetime(array[2], array[1], array[0], array[3], array[4], array[5]).timestamp())
-
-
-def getDrink(event):
-    return re.findall("(?<=;)[a-zA-Z é]*(?=&)", event)[0]
-
-
+# Sets up connection to gmail
 def setupConnection():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -53,19 +40,43 @@ def setupConnection():
     return build('gmail', 'v1', credentials=creds)
 
 
-def addAllUnreadDispensedDrinkEvent(gmail_con, db_conn):
-    # TODO Mark emails as unread. Or find better solution than to list all mails, perhaps also delete already-processed mails?
-    # TODO Also I think it only gets one page of mails, currently gets exactly 100
-    # Get all mails that are unread and have label dispenseddrinkevent. All mails containing the words "DispensedDrinkEvent"
-    # automatically gets this label when received
-    inbox = gmail_con.users().messages().list(userId='me', q='label:dispenseddrinkevent is:unread').execute()
+# Converts the timestamp in mail for dispenseEvent to unix timestamp
+def getUnixTime(time):
+    # Get all numbers from the snippet
+    array = list(map(int, re.findall(r'[0-9]+', time)))
+
+    # Convert to unix timestamp in correct order. Might be a better solution.
+    # (mail is in DD/MM/YY, function is in (YY/MM/DD, therefore the weird indexes)
+    return int(datetime.datetime(array[2], array[1], array[0], array[3], array[4], array[5]).timestamp())
+
+
+# Return the name of the first drink
+def getDrink(event):
+    return re.findall("(?<=;)[a-zA-Z é]*(?=&)", event)[0]
+
+
+def addUnreadDispensedDrinkEvent(gmail_con, db_conn, search):
+    # Get all mails that match the search
+    inbox = gmail_con.users().messages().list(userId='me', q=search, maxResults=20).execute()
+
     # Gets information from mails in "minimal" format, as only a snippet of the mail is needed (recent event is in start of mail)
     mails = [gmail_con.users().messages().get(userId="me", id=msg['id'], format="minimal").execute() for msg in
              inbox["messages"]]
 
-    # Add to db
-    for m in mails:
-        insert_event(db_conn, getUnixTime(m["snippet"]), "DispensedDrinkEvent", getDrink(m["snippet"]))
+    # Array to hold ids of mails to remove "unread"- label
+    idsToModify = []
+
+    for mail in mails:
+        # Add the DispensedDrinkEvent to db
+        insert_event(db_conn, getUnixTime(mail["snippet"]), "DispensedDrinkEvent", getDrink(mail["snippet"]))
+
+        # Append id to array
+        idsToModify.append(mail['id'])
+
+    # Remove "unread"- label on all mails in array
+    gmail_con.users().messages().batchModify(userId='me',
+                                             body={'removeLabelIds': ['UNREAD'], 'addLabelIds': [],
+                                                   'ids': idsToModify}).execute()
 
 
 def main():
@@ -75,9 +86,11 @@ def main():
     db_conn = sqlite3.connect('jvm-loss.db')
     create_db(db_conn, True, True)
 
-    # Get all unread DispensedDrinkEvent and add to database
     while True:
-        addAllUnreadDispensedDrinkEvent(gmail_con, db_conn)
+        # Once every 10 mins get all unread DispensedDrinkEvent and add to database
+        search = 'label:dispenseddrinkevent is:unread'
+        addUnreadDispensedDrinkEvent(gmail_con, db_conn, search)
+        print(len(get_events(db_conn)))
         time.sleep(600)
 
 
