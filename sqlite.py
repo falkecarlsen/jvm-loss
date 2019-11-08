c7b0793a8367d63720a49233b0a43857b3d8bb25
cogitantium <falke.cs@gmail.com>
2019-11-08 14:19:04 +0100

    Add database unittests and refactor sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -1,15 +1,18 @@
 import sqlite3
-from pprint import pprint
+import time
+import unittest
 
-table_name = "jvm_dispense_events"
+TABLE_NAME = "jvm_dispense_events"
+DISPENSE_EVENT = "DispensedDrinkEvent"
+TEST_DB_PATH = "test-databases"
 
 
 def create_db(conn: sqlite3.Connection, drop=False, create=False):
     cur = conn.cursor()
     if drop:
-        cur.execute(f'DROP TABLE IF EXISTS {table_name}')
+        cur.execute(f'DROP TABLE IF EXISTS {TABLE_NAME}')
 
-    create_table_query = (f'CREATE TABLE {table_name} (\n'
+    create_table_query = (f'CREATE TABLE {TABLE_NAME} (\n'
                           '                            timestamp INTEGER PRIMARY KEY,\n'
                           '                            event_type TEXT NOT NULL,\n'
                           '                            event_item TEXT NOT NULL)\n'
@@ -24,30 +27,70 @@ def create_db(conn: sqlite3.Connection, drop=False, create=False):
 
 def insert_event(conn: sqlite3.Connection, time: int, type: str, item: str):
     cur = conn.cursor()
-    insert_query = f'INSERT INTO {table_name} (timestamp, event_type, event_item) VALUES (?,?,?)'
+    insert_query = f'INSERT INTO {TABLE_NAME} (timestamp, event_type, event_item) VALUES (?,?,?)'
     cur.execute(insert_query, (time, type, item))
     conn.commit()
 
 
 def get_event(conn: sqlite3.Connection, time: int):
     cur = conn.cursor()
-    select_event_query = f'SELECT * FROM {table_name} WHERE "timestamp"=?'
+    select_event_query = f'SELECT * FROM {TABLE_NAME} WHERE "timestamp"=?'
     # Note, parameters must be iterable
     cur.execute(select_event_query, (time,))
     return cur.fetchall()
 
 
+def get_events_by_type(conn: sqlite3.Connection, type: str):
+    cur = conn.cursor()
+    select_event_query = f'SELECT * FROM {TABLE_NAME} WHERE "event_type"=?'
+    # Note, parameters must be iterable
+    cur.execute(select_event_query, (type,))
+    return cur.fetchall()
+
+
 def get_events(conn: sqlite3.Connection):
     cur = conn.cursor()
-    select_query = f'SELECT * FROM {table_name}'
+    select_query = f'SELECT * FROM {TABLE_NAME}'
     cur.execute(select_query)
     return cur.fetchall()
 
 
-db_conn = sqlite3.connect('jvm-loss.db')
-create_db(db_conn, True, True)
-for i in range(100):
-    insert_event(db_conn, i, "DispensedDrinkEvent", "Coffee")
-pprint(get_events(db_conn))
-print("Selecting for time=42 event")
-pprint(get_event(db_conn, 42))
+class TestDBFunctions(unittest.TestCase):
+    def setUp(self):
+        """
+        Setup test-dir if it doesn't exist yet, for putting testing databases into
+        :return:
+        """
+        import os
+        if not os.path.exists(TEST_DB_PATH):
+            os.makedirs(TEST_DB_PATH)
+
+    def test_database_creation(self):
+        """
+        Test that a database can be setup and that it is empty
+        :return:
+        """
+        db_conn = sqlite3.connect(f"{TEST_DB_PATH}/{self._testMethodName}.db")
+        create_db(db_conn, True, True)
+        # Assert empty list returned when getting all events
+        self.assertEqual(get_events(db_conn), [])
+
+    def test_database_insert_and_get(self):
+        """
+        Test that a single inserted event into an empty database functions as expected
+        :return:
+        """
+        db_conn = sqlite3.connect(f"{TEST_DB_PATH}/{self._testMethodName}.db")
+        create_db(db_conn, True, True)
+        # Insert event and assert for existence
+        event = "Strong Coffee with Milk"
+        timestamp = int(time.time())
+        insert_event(db_conn, timestamp, DISPENSE_EVENT, event)
+        # Assert that every element is as expected
+        self.assertEqual((get_event(db_conn, timestamp))[0][0], timestamp)
+        self.assertEqual((get_event(db_conn, timestamp))[0][1], DISPENSE_EVENT)
+        self.assertEqual((get_event(db_conn, timestamp))[0][2], event)
+
+
+if __name__ == '__main__':
+    unittest.main()
